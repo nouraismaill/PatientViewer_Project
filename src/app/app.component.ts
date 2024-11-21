f1d516a56a7443e0e1026d801d130b19ef90fe89
Noura Ismail <12030827@students.liu.edu.lb>
2024-11-21 22:54:46 +0200

    modify app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 
+import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { PatientsComponent } from './patients/patients.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, PatientsComponent],
+  imports: [HeaderComponent, PatientsComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
