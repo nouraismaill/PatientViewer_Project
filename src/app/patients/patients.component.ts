06a9418b319d85b792174f228382c685aed257da
Noura Ismail <12030827@students.liu.edu.lb>
2024-11-21 22:51:33 +0200

    added patient component

--- src/app/patients/patients.component.ts
+++ src/app/patients/patients.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
+
+import { FormsModule } from '@angular/forms';
+
+import { Patients } from './patient.model';
+import { PatientService } from './patient.service';
+import { PatientComponent } from './patient/patient.component';
 
 @Component({
   selector: 'app-patients',
   standalone: true,
-  imports: [],
+  imports: [PatientComponent, FormsModule],
   templateUrl: './patients.component.html',
-  styles: ``
+  styles: ``,
 })
-export class PatientsComponent {
+export class PatientsComponent implements OnInit {
+  patients = signal<Patients[]>([]); // Original list of patients
 
+  isFetching = signal(false);
+  error = signal('');
+
+  private destroyRef = inject(DestroyRef);
+  private patientService = inject(PatientService);
+
+  ngOnInit() {
+    this.isFetching.set(true);
+
+    const subscription = this.patientService.getAllPatients().subscribe({
+      next: (patients) => {
+        this.patients.set(patients);
+      },
+      error: (error) => {
+        console.error(error);
+        this.error.set(error.message);
+      },
+      complete: () => {
+        this.isFetching.set(false);
+      },
+    });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
 }
