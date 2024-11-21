b7cf58147946cd1044d3b626489c3b728c5954f4
Noura Ismail <12030827@students.liu.edu.lb>
2024-11-21 22:53:52 +0200

    added Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { AppointmentsComponent } from './appointments/appointments.component';
+import { NoAppointmentComponent } from './no-appointment/no-appointment.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ProfileComponent } from './profile/profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: NoAppointmentComponent,
+    title: 'No Appointment selected',
+  },
+  {
+    path: 'profile/:patientId',
+    component: ProfileComponent,
+    title: 'Profile',
+    children: [
+      {
+        path: 'appointments',
+        component: AppointmentsComponent,
+      },
+    ],
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+  },
+];
