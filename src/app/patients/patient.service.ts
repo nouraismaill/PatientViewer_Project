1a3b835e1638318bfbbe533278d20c82e5ff451f
Noura Ismail <12030827@students.liu.edu.lb>
2024-11-21 22:47:11 +0200

    added PatientService

--- src/app/patients/patient.service.ts
+++ src/app/patients/patient.service.ts
@@ -1,6 +1,31 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { catchError, map, Observable, throwError } from 'rxjs';
+import { Patients } from './patient.model';
 
 @Injectable({
   providedIn: 'root',
 })
-export class PatientService {}
+export class PatientService {
+  private httpClient = inject(HttpClient);
+
+  getAllPatients(): Observable<Patients[]> {
+    return this.fetchingPatients('http://localhost:3000/patients');
+  }
+  getPatientById(patientId: string): Observable<Patients> {
+    return this.httpClient
+      .get<{ patient: Patients }>(`http://localhost:3000/patient/${patientId}`)
+      .pipe(map((response) => response.patient));
+  }
+
+  private fetchingPatients(url: string) {
+    return this.httpClient.get<{ patients: Patients[] }>(url).pipe(
+      map((resData) => resData.patients),
+
+      catchError((error) => {
+        console.log('An error occurred while fetching patients:', error);
+        return throwError(() => new Error(error));
+      })
+    );
+  }
+}
