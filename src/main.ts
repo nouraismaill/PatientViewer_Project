61ce06808a44bad2b699068d274cd62e349fa283
Noura Ismail <12030827@students.liu.edu.lb>
2024-11-21 22:56:05 +0200

    modify icon

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,26 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+import {
+  HttpHandlerFn,
+  HttpRequest,
+  provideHttpClient,
+  withInterceptors,
+} from '@angular/common/http';
+import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
+function loggingInterceptor(
+  request: HttpRequest<unknown>,
+  next: HttpHandlerFn
+) {
+  console.log('[Outgoing Request]');
+  console.log(request);
+  return next(request);
+}
+
+bootstrapApplication(AppComponent, {
+  ...appConfig,
+  providers: [
+    ...(appConfig.providers || []),
+    provideHttpClient(withInterceptors([loggingInterceptor])),
+  ],
+}).catch((err) => console.error(err));
