a85ff1bad101256d8e300a164c63c9fbfb0df9f8
Noura Ismail <12030827@students.liu.edu.lb>
2024-11-21 22:50:51 +0200

    added noappointment component

---
+++ src/app/no-appointment/no-appointment.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-no-appointment',
+  standalone: true,
+  imports: [],
+  templateUrl: './no-appointment.component.html',
+  styles: ``
+})
+export class NoAppointmentComponent {
+
+}
